82da0bfb5268bfeea1eca754ae8ce20ff608494f
djfrancesco <pacullfrancois@gmail.com>
2020-01-22 14:41:48 +0100

    can now use a credential file path in the fetcher init

--- pyinspire/services.py
+++ pyinspire/services.py
@@ -4,7 +4,7 @@ import json
 
 def load_json_credentials(file_path):
     """ Loads username and password from a json file.
-	"""
+    """
     with open("inspire_credentials.json") as json_file:
         creds = json.load(json_file)
     credentials = {}
@@ -17,6 +17,11 @@ class Fetcher:
     """ Fetching weather data from Inspire web services (Meteo-France).
     """
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+    def __init__(self, username=None, password=None, credentials_file_path=None):
+        if credentials_file_path is None:
+            self.username = username
+            self.password = password
+        else:
+            credentials = load_json_credentials(credentials_file_path)
+            self.username = credentials["username"]
+            self.password = credentials["password"]
