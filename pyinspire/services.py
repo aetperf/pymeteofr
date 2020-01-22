b257328f211014a93a6566c3f00a2ea2138220f7
djfrancesco <pacullfrancois@gmail.com>
2020-01-22 12:50:46 +0100

    added a new function to load credentials

--- pyinspire/services.py
+++ pyinspire/services.py
@@ -1,4 +1,15 @@
 """Main module."""
+import json
+
+
+def load_json_credentials(file_path):
+    """ Loads username and password from a json file.
+	"""
+    with open("inspire_credentials.json") as json_file:
+        creds = json.load(json_file)
+    assert "username" in creds
+    assert "password" in creds
+    return creds
 
 
 class Fetcher:
@@ -8,4 +19,3 @@ class Fetcher:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        print(username, password)
