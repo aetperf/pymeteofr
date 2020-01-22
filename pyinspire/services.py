53897139ee2d5b70555970dca874d7f37df59058
djfrancesco <pacullfrancois@gmail.com>
2020-01-22 12:07:43 +0100

    style

--- pyinspire/services.py
+++ pyinspire/services.py
@@ -1,8 +1,10 @@
 """Main module."""
 
+
 class Fetcher:
     """ Fetching weather data from Inspire web services (Meteo-France).
     """
+
     def __init__(self, username, password):
         self.username = username
         self.password = password
