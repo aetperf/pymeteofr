572c158ee64bc08df860d339c123dd619b8a13c2
djfrancesco <pacullfrancois@gmail.com>
2020-01-22 12:07:20 +0100

    created a Fetcher class

--- pyinspire/services.py
+++ pyinspire/services.py
@@ -1,1 +1,9 @@
 """Main module."""
+
+class Fetcher:
+    """ Fetching weather data from Inspire web services (Meteo-France).
+    """
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        print(username, password)
