44ada1422dac0568c6397bb31202ef395b622e21
djfrancesco <pacullfrancois@gmail.com>
2020-01-23 14:18:19 +0100

    added the pandas dependency

--- setup.py
+++ setup.py
@@ -4,45 +4,45 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['requests', 'numpy', 'rasterio']
+requirements = ["requests", "numpy", "pandas", "rasterio"]
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="Francois Pacull",
-    author_email='francois.pacull@architecture-performance.fr',
-    python_requires='>=3.5',
+    author_email="francois.pacull@architecture-performance.fr",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Fetching weather data from Inspire web services (Meteo-France)",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='pyinspire',
-    name='pyinspire',
-    packages=find_packages(include=['pyinspire', 'pyinspire.*']),
+    keywords="pyinspire",
+    name="pyinspire",
+    packages=find_packages(include=["pyinspire", "pyinspire.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/djfrancesco/pyinspire',
-    version='0.1.0',
+    url="https://github.com/djfrancesco/pyinspire",
+    version="0.1.0",
     zip_safe=False,
 )
