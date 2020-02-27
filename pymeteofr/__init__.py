5d503186be721a3262c8f4527a7c107664237a53
djfrancesco <pacullfrancois@gmail.com>
2020-02-27 12:10:33 +0100

    now exposing class directly instead of module

--- pymeteofr/__init__.py
+++ pymeteofr/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for PyMeteoFr."""
 
-__author__ = """Francois Pacull"""
+__author__ = "François Pacull"
 __email__ = "francois.pacull@architecture-performance.fr"
 __version__ = "0.1.0"
+
+from .services import Fetcher
