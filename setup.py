2e633e3985533dce10c306a4bb467be2d9555a47
djfrancesco <pacullfrancois@gmail.com>
2021-02-15 17:35:30 +0100

    now only supporting python 3.7 and 3.8

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
